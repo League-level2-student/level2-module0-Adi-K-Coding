56b21d0c5653b0909c9ccd96afde78d28cfb810b
League Student <league@iMac-12.attlocal.net>
2019-12-08 16:01:18 -0800

    ArrayCheatSheet, RobotRace(Step Seven-Declare Winner of Race)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,47 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
+	public static void main(String[] args) {
 
-	//2. create an array of 5 robots.
+		// 2. create an array of 5 robots.
+		Robot[] robots = new Robot[5];
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-	//3. use a for loop to initialize the robots.
+		for (int i = 0; i < robots.length; i++) {
+			robots[i].setY(550);
+			robots[i].setX((i * 150) + 60);
+		}
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		Random r = new Random();
+		boolean isRacing=true;
+		while (isRacing==true) {
+		for (int i = 0; i < robots.length; i++) {
+			int x = r.nextInt(50);
+			robots[i].setSpeed(10);
+			robots[i].move(x);
+		if(robots[i].getY()<0) {
+			isRacing=false;
+		}
+		}
+		}
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+		// 7. declare that robot the winner and throw it a party!
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+		// 8. try different races with different amounts of robots.
 
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
+		// 9. make the robots race around a circular track.
+	}
 }

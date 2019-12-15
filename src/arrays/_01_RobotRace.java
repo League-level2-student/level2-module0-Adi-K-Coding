393ab0e18422eac1c48d7c4952ab97a7c03b70da
League Student <league@iMac-12.attlocal.net>
2019-12-15 15:59:59 -0800

    robot race, find the hidden button(IP-Step 13)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -17,24 +17,25 @@ public class _01_RobotRace {
 		// 4. make each robot start at the bottom of the screen, side by side, facing up
 
 		for (int i = 0; i < robots.length; i++) {
-			robots[i].setY(550);
-			robots[i].setX((i * 150) + 60);
+			robots[i].setY(400);
+			robots[i].setX(200);
 		}
 
 		// 5. use another for loop to iterate through the array and make each robot move
 		// a random amount less than 50.
-		Random r = new Random();
-		boolean isRacing=true;
-		while (isRacing==true) {
-		for (int i = 0; i < robots.length; i++) {
-			int x = r.nextInt(50);
-			robots[i].setSpeed(10);
-			robots[i].move(x);
-		if(robots[i].getY()<0) {
-			isRacing=false;
-		}
-		}
-		}
+//		Random r = new Random();
+//		boolean isRacing=true;
+//		while (isRacing==true) {
+//		for (int i = 0; i < robots.length; i++) {
+//			int x = r.nextInt(50);
+//			robots[i].setSpeed(10);
+//			robots[i].move(x);
+//		if(robots[i].getY()<0) {
+//			isRacing=false;
+//		System.out.println("Robot "+(i+1)+" Wins!");
+//		}
+//		}
+//		}
 		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
 		// screen.
 
@@ -43,5 +44,24 @@ public class _01_RobotRace {
 		// 8. try different races with different amounts of robots.
 
 		// 9. make the robots race around a circular track.
+		Random r = new Random();
+
+		boolean isRacing = true;
+		while (isRacing == true) {
+			for (int i = 0; i < robots.length; i++) {
+				robots[i].setSpeed(9999999);
+				robots[i].penDown();
+				int x = (r.nextInt(10) + 1);
+				for (int j = 0; j < x; j++) {
+					robots[i].move(3);
+					robots[i].turn(1);
+				}
+				if (robots[i].getY() < 405 && robots[i].getY() > 395 && robots[i].getX() < 210) {
+					isRacing = false;
+					System.out.println("Robot " + (i + 1) + " Wins!");
+				}
+			}
+		}
+
 	}
 }
